286cb6935ac52d4e57d781d640fff6f67368fd50
Charles Magiera <charles.magiera@sky.uk>
2024-08-23 15:25:01 +0100

    Added button and logic with it

--- app/src/main/java/com/example/strength4mom/MainActivity.kt
+++ app/src/main/java/com/example/strength4mom/MainActivity.kt
@@ -12,18 +12,27 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontStyle
 import com.example.strength4mom.data.Exo
 import com.example.strength4mom.data.exos
 import com.example.strength4mom.ui.theme.Strength4MomTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,6 +67,7 @@ fun StrengthApp() {
 }
 
 
+
 /**
  * Composable displaying the list of exercises with the image and information
  *
@@ -67,6 +77,7 @@ fun ExoItem(
     exo: Exo,
     modifier: Modifier = Modifier
 ) {
+
     Card(modifier = modifier) {
         Column(modifier = modifier) {
             Row(modifier = modifier) {
@@ -83,6 +94,8 @@ fun ExoInfo(
     exoReps: Int,
     modifier: Modifier = Modifier
 ) {
+    var expended by remember { mutableStateOf(false) }
+
     Column(
         modifier = modifier
             .fillMaxWidth()
@@ -104,7 +117,29 @@ fun ExoInfo(
                 modifier = modifier
                     .weight(1f)
             )
+            ExoButton(expended = expended, onClick = { expended = !expended })
         }
+    }
+}
+
+
+//Added Gradle dependency to be able to use icons from https://fonts.google.com/icons
+//Added implementation("androidx.compose.material:material-icons-extended") to to :app build.gradle.kts file
+@Composable
+private fun ExoButton(
+    expended: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    IconButton(
+        onClick = onClick,
+        modifier = modifier
+    ) {
+        Icon(
+            imageVector = if (!expended) Icons.Filled.ExpandMore else Icons.Filled.ExpandLess,
+            contentDescription = stringResource(R.string.expand_button),
+            tint = MaterialTheme.colorScheme.secondary,
+        )
     }
 }
 
