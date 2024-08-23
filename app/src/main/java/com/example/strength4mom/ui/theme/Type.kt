dabe394989a32f6640fce030795ffe221f3e38ed
Charles Magiera <charles.magiera@sky.uk>
2024-08-23 12:28:02 +0100

    Added Fonts to the app and updated Type.kt

--- app/src/main/java/com/example/strength4mom/ui/theme/Type.kt
+++ app/src/main/java/com/example/strength4mom/ui/theme/Type.kt
@@ -5,30 +5,40 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.Font
+import com.example.strength4mom.R
 
-// Set of Material typography styles to start with
+
+//Initializing imported font here
+//Need to import Font and FontFamily
+val LaoSansLao = FontFamily(
+    Font(R.font.notosanslao_regular),
+    Font(R.font.notosanslao_bold, FontWeight.Bold)
+)
+
+// Set of Material typography styles to start with with LaoSansLao being used
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = LaoSansLao,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontSize = 14.sp,
+//        lineHeight = 24.sp,
+//        letterSpacing = 0.5.sp
+    ),
+    displayLarge = TextStyle(
+        fontFamily = LaoSansLao,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 36.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = LaoSansLao,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = LaoSansLao,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
     )
-    */
 )
+
