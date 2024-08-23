403fa1a21655faf88b21d4dd0317b35b61cc1701
Charles Magiera <charles.magiera@sky.uk>
2024-08-23 13:16:02 +0100

    started main and added prototype image to readme

--- app/src/main/java/com/example/strength4mom/MainActivity.kt
+++ app/src/main/java/com/example/strength4mom/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.strength4mom.data.Exo
+import com.example.strength4mom.data.exos
 import com.example.strength4mom.ui.theme.Strength4MomTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +26,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Strength4MomTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    topBar = {
+
+                    },
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    StrengthApp()
                 }
             }
         }
@@ -31,9 +40,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun StrengthApp() {
+    LazyColumn() {
+        items(exos) {
+            ExoItem(
+                exo = it,
+                modifier = Modifier
+            )
+        }
+    }
+}
+
+
+/**
+ * Composable displaying the list of exercises with the image and information
+ *
+ */
+@Composable
+fun ExoItem(
+    exo: Exo,
+    modifier: Modifier = Modifier
+) {
+    Card(modifier = modifier) {
+        Column(modifier = modifier) {
+            Row(modifier = modifier) {
+                Text(text = "Test")
+            }
+        }
+    }
 }
